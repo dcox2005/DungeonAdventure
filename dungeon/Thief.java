3fa0c67c4aa1960d8b922543b28111d25fdfa1e1
darrikt <65573127+darrikt@users.noreply.github.com>
2020-05-21 09:58:29 -0700

    Update Thief.java
    removed battle choices as we moved it to the super class

--- dungeon/Thief.java
+++ dungeon/Thief.java
@@ -48,38 +48,5 @@ public class Thief extends Hero
 		
 	}//end surpriseAttack method
 
-    public void battleChoices(DungeonCharacter opponent)
-	{
-		super.battleChoices(opponent);
-		int choice;
-		do
-		{
-		    System.out.println("1. Attack Opponent");
-		    System.out.println("2. Surprise Attack");
-		    System.out.print("Choose an option: ");
-		    choice = Keyboard.readInt();
-		    switch (choice)
-		    {
-			    case 1: attack(opponent);
-			        break;
-			    case 2: surpriseAttack(opponent);
-			        break;
-			    default:
-			        System.out.println("invalid choice!");
-		    }//end switch
-
-/*			numTurns--;
-			if (numTurns > 0)
-			    System.out.println("Number of turns remaining is: " + numTurns);
-
-		} while(numTurns > 0);
-*/
-		    this.setNumTurns(this.getNumTurns() - 1);
-			if (this.getNumTurns() > 0)
-			    System.out.println("Number of turns remaining is: " + this.getNumTurns());
-
-		}while(this.getNumTurns() > 0);
-		    
-    }//end battleChoices
     
 }//end Thief class

ad44492c7004b38f8c79a85b3d59490bf5f10ac4
darrikt <65573127+darrikt@users.noreply.github.com>
2020-05-21 09:54:44 -0700

    Update Warrior.java
    remove battle choices

--- dungeon/Warrior.java
+++ dungeon/Warrior.java
@@ -51,39 +51,6 @@ public class Warrior extends Hero
 		super.attack(opponent);
 	}//end override of attack method
 
-    public void battleChoices(DungeonCharacter opponent)
-	{
-		int choice;
-		super.battleChoices(opponent);
-		do
-		{
-		    System.out.println("1. Attack Opponent");
-		    System.out.println("2. Crushing Blow on Opponent");
-		    System.out.print("Choose an option: ");
-		    choice = Keyboard.readInt();
-
-		    switch (choice)
-		    {
-			    case 1: attack(opponent);
-			        break;
-			    case 2: crushingBlow(opponent);
-			        break;
-			    default:
-			        System.out.println("invalid choice!");
-		    }//end switch
-
-/*			numTurns--;
-			if (numTurns > 0)
-			    System.out.println("Number of turns remaining is: " + numTurns);
-
-		} while(numTurns > 0);
-*/
-		    this.setNumTurns(this.getNumTurns() - 1);
-			if (this.getNumTurns() > 0)
-			    System.out.println("Number of turns remaining is: " + this.getNumTurns());
-
-		}while(this.getNumTurns() > 0);
-		
-    }//end battleChoices method
+   
 
 }//end Warrior class

7fe2f82d7d2c4dc3078bcfd0b93208ec8978f50b
Justin Plett <64932344+jmplett@users.noreply.github.com>
2020-06-08 18:45:27 -0700

    Addition of toString to PillarofOO

--- dungeon/PillarOfOO.java
+++ dungeon/PillarOfOO.java
@@ -2,14 +2,15 @@ package dungeon;
 
 public class PillarOfOO implements BackPackItem 
 {
-	public PillarOfOO()
-	{
-		
-	}//end Constructor
+
 	
 	public void useItem(Hero hero)
 	{
-		// TODO Auto-generated method stub
+		
 	}//end useItem()
+	
+	public static String toString() {
+		return "This is a pillar of OO you need four to use a pillar of OO";
+	}
 
 }//end PillarOfOO Class
